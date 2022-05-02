b8b097d1f62646851576d6ab8def53130cf4b75b
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2022-05-03 02:31:57 +0300

    improved

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,59 @@
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert (page > 0) and (page_size > 0)
+        # assert 
+        self.dataset()
+        start,end = self.index_range(page,page_size)
+        return self.__dataset[start:end]
+
+
+            
+    def index_range(self,page, page_size):
+        """returns a tuple"""
+        return ((page-1) * page_size, page * page_size)
+
+
+server = Server()
+
+try:
+    should_err = server.get_page(-10, 2)
+except AssertionError:
+    print("AssertionError raised with negative values")
+
+try:
+    should_err = server.get_page(0, 0)
+except AssertionError:
+    print("AssertionError raised with 0")
+
+try:
+    should_err = server.get_page(2, 'Bob')
+except AssertionError:
+    print("AssertionError raised when page and/or page_size are not ints")
+
+print(server.get_page(1, 3))
+print(server.get_page(3, 2))
+print(server.get_page(3000, 100))
