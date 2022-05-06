160b2e5bf84775e77eec40ba9d55818bd0dba913
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2022-05-06 23:35:39 +0300

    all

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -33,6 +33,29 @@ class Server:
         [start, end] = index_range(page, page_size)
         return self.dataset()[start: end]
 
+    def get_hyper(self, page: int, page_size: int) -> dict:
+        """
+        Retuns a dictionary of:
+        page_size: the length of the returned dataset page
+        page: the current page number
+        data: the dataset page (equivalent to return from previous task)
+        next_page: number of the next page, None if no next page
+        prev_page: number of the previous page, None if no previous page
+        total_pages: the total number of pages in the dataset as an integer
+        """
+        dataset_records = self.get_page(page, page_size)
+
+        page_dict = math.ceil(len(self.__dataset) / page_size)
+
+        return {
+            'page_size': len(dataset_records),
+            'page': page,
+            'data': dataset_records,
+            'next_page': page + 1 if (page + 1) <= page_dict else None,
+            'prev_page': page - 1 if (page - 1) > 0 else None,
+            'total_pages': page_dict
+        }
+
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
