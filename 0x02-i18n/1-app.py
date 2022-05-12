945d1f6e8177e22f6a7c198b3060bda9d3e2520f
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2022-05-12 16:04:46 +0300

    task 1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,20 @@
+""" sth unknown"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config():
+    """config class"""
+    LANGUAGES = ["en", "fr"]
+
+
+app = Flask(__name__)
+app.config['BABEL_DEFAULT_LOCALE'] = Config.LANGUAGES[1]
+app.config['BABEL_DEFAULT_TIMEZONE'] = "UTC"
+babel = Babel(app)
+
+
+@app.route('/')
+def hello():
+    """func"""
+    return render_template('1-index.html')
